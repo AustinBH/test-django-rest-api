c2b2493800b5382c412dcd7cf20a990fcf80f3a2
AustinBH <harlowaustin2@gmail.com>
2019-09-23 12:58:11 -0700

    added permissions for the API, also completed part 4 of the django_rest_framework tutorial

--- tutorial/quickstart/serializers.py
+++ tutorial/quickstart/serializers.py
@@ -1,22 +1,21 @@
-from django.contrib.auth.models import User, Group
+# from django.contrib.auth.models import User, Group
 # from models import Post
-from rest_framework import serializers
+# from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['url', 'username', 'email', 'groups']
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
+# class GroupSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Group
+#         fields = ['url', 'name']
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
-    pass
-    # class Meta:
-    #     model = Post
-    #     fields = ['url', 'content', 'user_id', 'pub_date']
+# class PostSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Post
+#         fields = ['url', 'content', 'user_id', 'pub_date']
--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,28 +1,21 @@
 from django.contrib.auth.models import User, Group
 # from tutorial.quickstart.models import Post
 from rest_framework import viewsets
-from tutorial.quickstart.serializers import UserSerializer, GroupSerializer, PostSerializer
+# from tutorial.quickstart.serializers import UserSerializer, GroupSerializer, PostSerializer
 
 # Create your views here.
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-
-# class PostViewSet(viewsets.ModelViewSet):
+# class UserViewSet(viewsets.ModelViewSet):
 #     """
-#     API endpoint that allows posts to be viewed or edited
+#     API endpoint that allows users to be viewed or edited
 #     """
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = UserSerializer
+
+
+# class GroupViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows groups to be viewed or edited
+#     """
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer
